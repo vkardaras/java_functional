851f39ee6071d7e46d52aaa184f3b6fcbf5e2e2e
vkardaras <vasiliskardaras@gmail.com>
2021-08-25 18:20:23 +0300

    initial commit

---
+++ src/main/java/streams/_Streams.java
@@ -0,0 +1,59 @@
+package streams;
+
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static streams._Streams.Gender.*;
+
+public class _Streams {
+
+    public static void main(String[] args) {
+
+        List<Person> people = List.of(
+                new Person("John", MALE),
+                new Person("Maria", FEMALE),
+                new Person("Aisha", FEMALE),
+                new Person("Alex", MALE),
+                new Person("Alice", FEMALE)
+        );
+
+        people.stream()
+                .map(person -> person.gender)
+                .collect(Collectors.toSet())
+                .forEach(System.out::println);
+
+        people.stream()
+                .map(person -> person.name)
+                .mapToInt(String::length)
+                .forEach(System.out::println);
+
+        Predicate<Person> personPredicate = person -> FEMALE.equals(person.gender);
+        boolean containsOnlyFemales = people.stream()
+                .allMatch(personPredicate);
+        System.out.println(containsOnlyFemales);
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
+}
